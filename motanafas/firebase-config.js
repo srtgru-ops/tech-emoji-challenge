6fd49a83469ed1253b8d23d6567a48a545197847
srtgru-ops <srtgru@gmail.com>
2026-09-24 15:44:58 +0300

    Add Motanafas Firebase app

---
+++ motanafas/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArED23czTOuU1_lt4C13-wzNKFfZGdK3o",
+  authDomain: "motanafas.firebaseapp.com",
+  projectId: "motanafas",
+  storageBucket: "motanafas.firebasestorage.app",
+  messagingSenderId: "1075049732341",
+  appId: "1:1075049732341:web:fdcf05bb22c0c5b850cdbc"
+};
